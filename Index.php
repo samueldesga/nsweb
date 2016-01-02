5abcff0b4e66eec1971b4e9f90df94de759d3910
boolicool <1364237@etu.cegep-ste-foy.qc.ca>
2016-01-02 15:43:49 -0500

    Ajout de bootstrap, pour l'instant y'a rien d'affiche

--- Index.php
+++ Index.php
@@ -1,8 +1,27 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    <title>NsWeb</title>
 
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap-3.3.6-dist/bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+      <div class="container">
+          test
+      </div>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  </body>
+</html>
