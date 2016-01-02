1a99058d21e64e4d3d6b1f167160a752ca57e020
boolicool <1364237@etu.cegep-ste-foy.qc.ca>
2016-01-02 16:08:19 -0500

    Integration de notre ancienne navbar. pas toute fonctionnelle
    Ya l'image qui devrait apparaitre (celle avec des barre, sur laquelle on
    clique lorsqu'on est sur mobile pour faire apparaitre le menu) qui
    n'apparait pas, tout fonctionne bien sinon

--- Index.php
+++ Index.php
@@ -2,6 +2,8 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
+    <?php include('navbar.html');  ?>
+    
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
@@ -11,6 +13,8 @@
 
     <!-- Bootstrap core CSS -->
     <link href="bootstrap-3.3.6-dist/bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="stylesheet.css" rel="stylesheet">
+    
 
   </head>
 
@@ -23,5 +27,6 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="script.js"></script>
   </body>
 </html>
