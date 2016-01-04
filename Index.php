4df58c889d830122f5a4e615b761e8ec2bf3a080
boolicool <1364237@etu.cegep-ste-foy.qc.ca>
2016-01-04 11:01:21 -0500

    Ajout de fontawesome + navbar fonctionnelle

--- Index.php
+++ Index.php
@@ -22,6 +22,7 @@
 
       <div class="container">
           test
+          <img src="images/nav-icon.png" class="nav-icon"/>
       </div>
     <!-- Bootstrap core JavaScript
     ================================================== -->
