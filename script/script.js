87b39c87f44b03e1b46236daad6ae920baf87405
Samuel Desgagnes <samueldesga@gmail.com>
2017-08-19 10:23:39 -0400

    afsd

---
+++ script/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){  
+  $("a").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 2000);
+  });
+});
